dd3c89c17a7ca74b8349a2bb769868bca056be18
kamrul95 <kamrulislam.bd95@gmail.com>
2024-09-21 19:02:07 +0600

    add theme structure

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>" class="no-js">
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body class="<?php body_class(); ?>">
+    <div id="header_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php wp_footer(); ?>
+</body>
+</html>
